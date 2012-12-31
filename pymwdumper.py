38d3260ffad9dc52ce221b9dafeea8d5d7196b5e
Chunliang Lu <aptenom@gmail.com>
2012-12-31 22:45:25 +0800

    support multiple version of dump file.

--- pymwdumper.py
+++ pymwdumper.py
@@ -1,38 +1,66 @@
 #!/usr/bin/env python
 
-
+import re
 import sys
 from lxml import etree
 
-NS = {
-    'xmlns': 'http://www.mediawiki.org/xml/export-0.7/',   
-}
-
-class MWDumper(object):
+class MWDump(object):
     """Read MediaWiki XML dump file
     """
 
     def __init__(self, filename):
         """
         """
-        self.xmlns = '{http://www.mediawiki.org/xml/export-0.7/}'
         self.filename = filename
-        pass
+        self.getns()
+
+    def getns(self):
+        """get xmlns
+        """
+        context = etree.iterparse(self.filename, events=('start-ns',))
+        for action, obj in context:
+            # we only care the declaration at firt line
+            break
+
+        if not obj or not obj[1].startswith('http://www.mediawiki.org/xml/export'):
+            print "Cannot find valid xmlns declarations."
+            sys.exit()
+
+        self.ns = obj[1]
+        match = re.match(r'http://www.mediawiki.org/xml/export-(\d\.\d)/', self.ns)
+        self.version = float(match.group(1))
 
     def iterpages(self):
+        """iterate all the pages in this xml file
         """
-        """
-        context = etree.iterparse(self.filename, events=('end',), tag=self.xmlns+'page') 
+        context = etree.iterparse(self.filename, events=('end',), tag='{%s}page' % self.ns) 
         for event, element in context:
 
             page = {}
-            page['ns'] = element.findtext(self.xmlns+'ns')
-            page['id'] = element.findtext(self.xmlns+'id')
-            page['title'] = element.findtext(self.xmlns+'title')
-            page['revision_id'] = element.findtext(self.xmlns+'revision/'+self.xmlns+'id')
-            page['revision_text'] = element.findtext(self.xmlns+'revision/'+self.xmlns+'text')
+            page['id'] = element.findtext('{%s}id' % self.ns)
+            page['title'] = element.findtext('{%s}title' % self.ns)
+            # currently I only care about the text
+            page['text'] = element.findtext('{%s}revision/{%s}text' % (self.ns, self.ns))
+            #page['revision'] = {}
+            #page['revision_id'] = element.findtext('{%s}revision/{%s}id' % (self.ns, self.ns))
+            #page['revision_text'] = element.findtext('{%s}revision/{%s}text' % (self.ns, self.ns))
+            
+            if self.version >= 0.6:
+                # ns field starts from version 0.6
+                page['ns'] = element.findtext('{%s}ns' % self.ns)
+                
+            if self.version >= 0.3:
+                redirect = element.find('{%s}redirect' % self.ns)
+                if redirect is not None:
+                    if self.version >= 0.6:
+                        # starting from version 0.6, the redirect target will be in `title` attribute
+                        page['redirect'] = redirect.attrib['title']
+                    else:
+                        # previous versions need to find redirect target from text
+                        #match = re.search(r'\[\[(.*)\]\]', page['revision_text'])
+                        match = re.search(r'\[\[(.*)\]\]', page['text'])
+                        page['redirect'] = match.group(1) if match else 'NOTFOUND'
     
-
             yield page
             
             # clear the element
@@ -59,10 +87,7 @@ class MWDumper(object):
                 del element.getparent()[0]
     
         del context
-
         return count
-
-       
     
 def main():
     if len(sys.argv) < 2:
@@ -70,13 +95,17 @@ def main():
         sys.exit(0)
     
     xml_filename = sys.argv[1]
-    mwdumper = MWDumper(xml_filename)
-    for page in mwdumper.iterpages():
-        print page
-        break
+    mwdump = MWDump(xml_filename)
+    count = 0
+    for page in mwdump.iterpages():
+        print page['id'], page['title'], page['redirect'] if 'redirect' in page else 'NOREDIRECT'
 
-    count = mwdumper.countpages()
-    print "number of pages in this xml:", count
+        count += 1
+        if count > 1000:
+            break
+
+    #count = mwdump.countpages()
+    #print "number of pages in this xml:", count
 
 
 if __name__ == '__main__':
